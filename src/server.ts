10ff1ea8f527fe5c3a850e20e433307075af4942
Thulasi, Harsha (ht542y) <ht542y@att.com>
2020-02-08 14:59:29 -0800

    Updating to latest.  Adding the server.js to connect to mongo and the tsconfig with using es6 target.

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import * as express from "express";
+import * as mongoose from "mongoose";
+
+let app = express();
+const port = 8080; // default port to listen
+
+
+const mongoDbUrl = process.env.CUSTOMCONNSTR_mongoDbConnStr || 'mongodb://localhost:27017/twitchFighter';
+
+
+// start the Express server
+app.listen(port, async () => {
+    const options: mongoose.ConnectionOptions = {
+      useNewUrlParser: true,
+    }
+    await mongoose.connect(mongoDbUrl, options);
+    const collections = await mongoose.connection.db.collections();
+    collections.forEach(c => console.log(`Found: ${c.collectionName}`));
+    console.log(`server is ready to use at http://localhost:${port}`);
+  });
+
+
+// // start the Express server
+// app.listen( port, () => {
+//     // tslint:disable-next-line:no-console
+//     console.log( `server started at http://localhost:${ port }` );
+// } );
