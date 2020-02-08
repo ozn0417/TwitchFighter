dcff3d8ec2fee7fda09502ec264ef951e9e2d049
Thulasi, Harsha (ht542y) <ht542y@att.com>
2020-02-08 13:53:36 -0800

    Updating few more comments.

--- src/models/stream.model.ts
+++ src/models/stream.model.ts
@@ -10,13 +10,18 @@ import mongoose, { Schema, Document } from 'mongoose';
  *       twitchUserName:
  *         type: string
  *         example: 'myTwitchName'
+ *       streamUrl:
+ *         type: string
+ *         example: 'https://twitch.com/s/myTwitchName'
  */
 export interface Stream extends Document {
     twitchUserName: string;
+    streamUrl: string;
 }
 
 const StreamSchema: Schema = new Schema({
-    twitchUserName: { type: String, required: true }
+    twitchUserName: { type: String, required: true },
+    streamUrl: { type: String, required: true}
 });
 
 export default mongoose.model<Stream>('Stream', StreamSchema, 'streams');
